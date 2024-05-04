068afc846e67e7d0719467b9f394877128fa379f
Amin <aminshiravand92@gmail.com>
2024-05-04 06:23:45 -0700

    --SingleTone Utils added

---
+++ Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utils
+{
+    public class Singleton<T> where T : class, new()
+    {
+        private static readonly object LockObject = new object();
+        private static T s_instance;
+
+        public static T Instance
+        {
+            get
+            {
+                lock( LockObject )
+                {
+                    if( s_instance != null )
+                    {
+                        return s_instance;
+                    }
+
+                    s_instance = new T();
+                    ( s_instance as Singleton<T> )?.Init();
+                    return s_instance;
+                }
+            }
+        }
+
+
+        public static void DestroyInstance()
+        {
+            lock( LockObject )
+            {
+                s_instance = null;
+            }
+        }
+
+        protected Singleton() { }
+        protected virtual void Init() { }
+    }
+}
